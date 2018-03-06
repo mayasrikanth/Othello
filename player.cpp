170ed0db2e6e10d2852b3a1984e8dbf29d574830
mayasrikanth <mayassrikanth@gmail.com>
2018-03-05 22:20:18 -0800

    score system part 1

--- player.cpp
+++ player.cpp
@@ -52,7 +52,25 @@ Move *Player::doMove(Move *opponentsMove, int msLeft) {
      * TODO: Implement how moves your AI should play here. You should first
      * process the opponent's opponents move before calculating your own move
      */
-     othello->doMove(opponentsMove, yours);
+
+    othello->doMove(opponentsMove, yours);
+     Move *move = new Move(0, 0);
+     for (int i = 0; i < 8; i ++)
+     {
+       for (int j = 0; j < 8; j ++)
+       {
+         move ->setX(i);
+         move ->setY(j);
+         if (othello->checkMove(move, mine))
+         {
+           othello->doMove(move, mine);
+           //return move;
+         }
+       }
+     }
+
+
+     /* othello->doMove(opponentsMove, yours);
      Move *move = new Move(0, 0);
      for (int i = 0; i < 8; i ++)
      {
@@ -67,6 +85,8 @@ Move *Player::doMove(Move *opponentsMove, int msLeft) {
          }
        }
      }
+     */
     //refer to minimax tree to determine the most favorable move
     return nullptr;
 }
+
