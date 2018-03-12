c179d9bd4cd5f399b634403a9b5dd5d0837fc7c2
Emma Qian <qianhanmin99@gmail.com>
2018-03-12 04:58:28 -0700

    changed get() in board

--- board.cpp
+++ board.cpp
@@ -252,28 +252,17 @@ int Board::getBoardScore(Side side)
    {
      for (int j = 0; j < 8; j ++)
      {
-        if(side == BLACK)
-    {
        if (get(side, i, j)) {
          temp->setX(i);
          temp->setY(j);
          score += checkScore(temp);
-     }
-     else if(!get(side, i, j) && occupied(i,j))
-     {
-        if (get(side, i, j)) {
-         temp->setX(i);
-         temp->setY(j);
-         score += checkScore(temp);
-
-     }
 
        }
      }
    }
    return score;
 }
-}
+
 
 /*
  * Sets the board state given an 8x8 char array where 'w' indicates a white
